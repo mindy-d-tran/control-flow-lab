c0ec68b41f7da4a2bd75a50313e921f3fe357e3d
Mindy D Tran <mindytran98@gmail.com>
2023-11-16 11:58:38 -0500

    create function to calculate space plants will occupy

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//Part One
+console.log("===== Part 1 =====")
+// Declaring variables
 // contains radius of area the plants
-const plantAreaRadius = 5;
-const plantArea = Math.PI *radius ** 2;
+const plantRadius = 5;
+//calculate max capacity of plant area
+const maxCapacity = Math.PI * plantRadius ** 2;
+// contains area of one plant 
+const plantArea = 0.8;
+
+// creating functions
+// calculate how many much space the plants will take in x weeks
+const plantGrowthSpace = (weeks) => {
+    // console.log(plantArea * 2 ** weeks);
+    return plantArea * 2 ** weeks;
+}
+
+// function plantDecision () {
+//     if() {
+
+//     }
+// }
